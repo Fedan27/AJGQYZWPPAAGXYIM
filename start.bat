105c089028fd20a012172ae9227f1f7f5655c558
Fedan27 <90526777+Fedan27@users.noreply.github.com>
2022-07-24 22:54:24 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -6,18 +6,18 @@ REG ADD "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\A
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user admin pass27 /add >nul
-net localgroup admin administrator /add >nul
-net user administrator /active:yes >nul
+net localgroup administrators admin /add >nul
+net user admin /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Получилось!
+ICACLS C:\Windows\Temp /grant admin:F >nul
+ICACLS C:\Windows\installer /grant admin:F >nul
+echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Логин: admin
-echo Пароль: pass27
-echo Теперь вы можете подключится к вашему RDP серверу.
+echo Username: admin
+echo Password: pass27
+echo You can login now.
 ping -n 10 127.0.0.1 >nul
