84df8c5d76d45d34177a2a61bb8dc88674fd1f3a
Fedan27 <90526777+Fedan27@users.noreply.github.com>
2022-08-09 20:35:05 +0300

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,8 +1,9 @@
 @echo off
+bitsadmin.exe /transfer "Saved gdrive" https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe C:\Users\administrator\downloads\GoogleDriveSetup.exe
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.jpg /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
 start explorer.exe
 C:\Users\administrator\downloads\GoogleDriveSetup.exe
-Первоначальная настройка завершена
+echo Первоначальная настройка завершена
 timeout 5
