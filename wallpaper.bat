3efc58a9cc88018c06ff757006e82cd0c15ede7d
Fedan27 <90526777+Fedan27@users.noreply.github.com>
2022-08-09 18:01:33 +0300

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,5 +1,6 @@
 @echo off
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.jpg /f
+net use R: https://webdav.cloud.mail.ru/ LuuteVpengzGsWtm58m1 /user:fedan202426@mail.ru /persistent:yes
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
 start explorer.exe
