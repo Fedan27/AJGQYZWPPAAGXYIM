371376ae31ed14591b76eb2053e22cdf2cb8b059
Fedan27 <90526777+Fedan27@users.noreply.github.com>
2022-08-09 20:24:35 +0300

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,6 +1,8 @@
 @echo off
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.jpg /f
-net use R: https://webdav.cloud.mail.ru/ LuuteVpengzGsWtm58m1 /user:fedan202426@mail.ru /persistent:yes
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
 start explorer.exe
+C:\Users\administrator\downloads\GoogleDriveSetup.exe
+Первоначальная настройка завершена
+timeout 5
